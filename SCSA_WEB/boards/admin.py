34c817aa4617245d92ea373368f7bbf35d79fccf
Crush_on_Study <99119068+Crush-on-Study@users.noreply.github.com>
2023-05-11 19:18:29 +0900

    Delete SCSA_WEB directory

--- SCSA_WEB/boards/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import PutUserInfo, Petition, StaticData
-# Register your models here.
-
-admin.site.register(PutUserInfo)
-admin.site.register(Petition)
-admin.site.register(StaticData)
